4e00c789ffac88989b22c6cb40131fd36aa06e20
Mostafa Mahmoud <mostafa416202@fsc.bu.edu.eg>
2025-08-25 17:01:13 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require('express')
+const cookieParser = require('cookie-parser');
+
+const { handleNotFound, globalError } = require('./middlewares/globalErrorHandler');
+
+// Import routes
+const authRoute = require('./routes/auth.route');
+const userRoute = require('./routes/user.route');
+
+
+const app = express()
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/users',userRoute)
+
+app.use(handleNotFound);
+app.use(globalError);
+
+module.exports = app
---
+++ routes/auth.route.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { body } = require("express-validator");
+
+const {
+  signup,
+  signin,
+  verifyEmail,
+  callback,
+  signout,
+  forgetPassword,
+  resetPassword,
+  resendVerification,
+} = require("../controllers/auth.controller");
+const { emailValidator } = require("../middlewares/validators");
+
+const router = express.Router();
+
+router.post(
+  "/signup", emailValidator,signup
+);
+router.post("/signin",  emailValidator, signin);
+router.post("/callback", callback);
+router.post("/verify-email", verifyEmail);
+router.post("/resend-verification", emailValidator, resendVerification);
+router.post("/signout",signout)
+
+router.post("/forget-password", emailValidator, forgetPassword);
+router.post("/reset-password", resetPassword);
+
+router.post("/new-access-token", newAccessToken);
+
+module.exports = router;
